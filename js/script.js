90e5d94ea35bb5dc341c20b918d4656eb49eab6d
TheDanielMcGraw <tincopper2@bitbucket.org>
2018-10-02 22:39:40 -0500

    corrected sentence start bolding

--- js/script.js
+++ js/script.js
@@ -1,12 +1,14 @@
 $(document).ready(function(){
   var body = document.getElementsByTagName("BODY")[0];
+  $('body').wrapInner('<p />');
   $('p').wrapAll('<div id="content" />');
         var request = new XMLHttpRequest();
         request.open('GET', 'js/navbar.html', false);
         request.send();
         var textfileContent = request.responseText;
 body.innerHTML += textfileContent;
-var t = $("#content").children("p").text();
-var size = t.match( /[^\.!\?]+[\.!\?]+/g )[1].length.valueOf();
-$("#content").children("p").html("<strong>" +t.match( /[^\.!\?]+[\.!\?]+/g )[1]+"</strong>"+ t.substring(parseInt(size)+2,t.length));
+var t = $("#content").children("p").text().trimStart();
+var size = t.match( /[^\.!\?]+[\.!\?]+/g )[0].length.valueOf();
+
+$("#content").children("p").html("<strong>" +t.match( /[^\.!\?]+[\.!\?]+/g )[0]+"</strong>"+ t.substring(size,t.length));
 });
